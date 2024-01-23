2f8303e08c5b8d14d4eb194da41783b7e8808f95
Muhammad Januar <muhammad.januar@modena.com>
2024-01-23 14:13:31 +0700

    fixing styling

--- src/pages/BookPage.jsx
+++ src/pages/BookPage.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useRef, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { CheckCircleIcon } from "@heroicons/react/20/solid";
 import Swal from "sweetalert2";
 import TimeRange from "../components/TimeRange";
@@ -160,26 +160,26 @@ export const BookPage = () => {
   }, []);
 
   return (
-    <div className="container mx-auto sm:px-6 lg:px-8">
+    <div className="container mx-auto max-w-7xl px-6 lg:px-8">
       {/* heading */}
       <div className="md:flex md:items-center md:justify-between">
-        <div className="min-w-0 flex-1">
+        <div className="min-w-0 flex-1 py-4">
           <h2 className="m-3 text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
-            Booking Ruang
+            Room Booking
           </h2>
         </div>
       </div>
       {/* heading */}
       {/* menu */}
-      <div className="bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2 p-10">
-        <div className="grid grid-cols-2 gap-4 mb-10">
+      <div className="bg-white shadow-lg ring-1 ring-gray-900/5 sm:rounded-xl rounded-xl md:col-span-2 p-10">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-6">
           <div>
             <div className="relative">
               <label
                 htmlFor="gedung"
                 className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
               >
-                Gedung
+                Building
               </label>
               <select
                 className="block w-full rounded-md border-0 py-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -187,21 +187,20 @@ export const BookPage = () => {
                 onChange={handleGedungChange}
                 ref={input.selectedGedung}
               >
-                <option value="0">Pilih Gedung</option>
+                <option value="0">Choose Building</option>
                 {/* Map over gedung options and render <option> elements */}
                 {gedung &&
                   gedung.map((g, index) => <option key={index}>{g}</option>)}
               </select>
             </div>
           </div>
-
           <div>
             <div className="relative">
               <label
                 htmlFor="ruang"
                 className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
               >
-                Ruang
+                Room
               </label>
               <select
                 className="block w-full rounded-md border-0 py-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -212,7 +211,7 @@ export const BookPage = () => {
                 {ruang &&
                   ruang.map((r, index) => (
                     <option key={index} value={r.id}>
-                      {r.nama_ruang} - ({r.kapasitas} orang)
+                      {r.nama_ruang} - ({r.kapasitas} Persons)
                     </option>
                   ))}
               </select>
@@ -220,14 +219,14 @@ export const BookPage = () => {
           </div>
         </div>
 
-        <div className="grid grid-cols-2 gap-4 mb-10">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-6">
           <div>
             <div className="relative">
               <label
                 htmlFor="name"
                 className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
               >
-                Keperluan Booking
+                Requirement of Booking
               </label>
               <input
                 type="text"
@@ -245,7 +244,7 @@ export const BookPage = () => {
                 htmlFor="name"
                 className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
               >
-                Tanggal Booking
+                Date of Booking
               </label>
               <input
                 type="date"
@@ -268,15 +267,22 @@ export const BookPage = () => {
                 htmlFor="name"
                 className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
               >
-                Waktu Booking
+                Start Booking Time
               </label>
-
               <TimeRange changeTime={handleTimeChange} />
             </div>
           </div>
         </div>
 
-        <div className="flex justify-end">
+        <div className="flex justify-end gap-4">
+          <NavLink to={"/"}>
+            <button
+              type="button"
+              className="inline-flex items-center rounded-md bg-gray-500 px-3.5 py-2.5 text-sm font-semibold shadow-sm hover:bg-gray-600 text-white"
+            >
+              Cancel
+            </button>
+          </NavLink>
           <button
             type="button"
             className="inline-flex items-center gap-x-2 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Calendars } from "../components/Calendars";
 
 export const DashboardPage = () => {
   const [role, setRole] = useState(null);
@@ -23,12 +24,20 @@ export const DashboardPage = () => {
 
   return (
     <>
-      <div className="container mx-auto sm:px-6 lg:px-8">
+      <div className="container mx-auto sm:px-6 lg:px-8 px-4">
+        {/* <div className="mx-auto max-w-7xl lg:px-8">
+        <div className="min-w-0 flex-1">
+            <h2 className="m-3 mt-5 mb-0 text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+              Booking Calendars
+            </h2>
+          </div>
+          <Calendars />
+        </div> */}
         {/* heading */}
         <div className="mx-auto max-w-7xl lg:px-8">
           <div className="min-w-0 flex-1">
             <h2 className="m-3 mt-5 mb-0 text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
-              Booking Room Menu
+              Room Booking Menu
             </h2>
           </div>
         </div>
@@ -36,24 +45,24 @@ export const DashboardPage = () => {
         {/* menu */}
         <div className="bg-white py-8 sm:py-8">
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
-            <div className="-mx-6 grid grid-cols-2 gap-4 overflow-hidden sm:mx-0 sm:rounded-2xl md:grid-cols-2">
+            <div className="-mx-6 grid grid-cols-2 gap-4 overflow-hidden sm:mx-0 sm:rounded-2xl rounded-2xl md:grid-cols-2">
               <div
-                className="bg-gray-400/5 p-8 sm:p-10 hover:bg-gray-800/5 col-span-full sm:col-span-1 cursor-pointer transition ease-in-out delay-150 hover:-translate-x-2 hover:scale-105 duration-300"
+                className="bg-gray-200 p-8 sm:p-10 hover:bg-gray-300 col-span-full sm:col-span-1 cursor-pointer transition ease-in-out delay-150 hover:-translate-x-2 hover:scale-105 duration-300"
                 onClick={clickBook}
               >
                 <img
                   className="max-h-12 w-full object-contain"
                   src="https://cdn-icons-png.flaticon.com/512/55/55281.png"
-                  alt="Transistor"  
+                  alt="Transistor"
                   width={158}
                   height={48}
                 />
                 <h3 className="text-center mt-2 text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
-                  Booking Room
+                  Room Booking
                 </h3>
               </div>
               <div
-                className="bg-gray-400/5 p-6 sm:p-10 hover:bg-gray-800/5 col-span-full sm:col-span-1 cursor-pointer transition ease-in-out delay-150 hover:translate-x-2 hover:scale-105 duration-300"
+                className="bg-gray-200 p-6 sm:p-10 hover:bg-gray-300 col-span-full sm:col-span-1 cursor-pointer transition ease-in-out delay-150 hover:translate-x-2 hover:scale-105 duration-300"
                 onClick={clickList}
               >
                 <img
@@ -68,7 +77,7 @@ export const DashboardPage = () => {
                 </h3>
               </div>
               <div
-                className="bg-gray-400/5 p-6 sm:p-10 col-span-full hover:bg-gray-800/5 cursor-pointer transition ease-in-out delay-150 hover:scale-105 duration-300"
+                className="bg-gray-200 p-6 sm:p-10 col-span-full hover:bg-gray-300 cursor-pointer transition ease-in-out delay-150 hover:scale-105 duration-300"
               >
                 <img
                   className="max-h-12 w-full object-contain"
